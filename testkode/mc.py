617075ff136f63b9fb3cbde73e227f3239560108
Odd Petter Sand <o.p.sand@astro.uio.no>
2025-06-13 12:20:23 +0200

    oppdatert nivå 1 & 3

--- testkode/mc.py
+++ testkode/mc.py
@@ -2,6 +2,7 @@
 
 # Nivå 1: Forklare
 
+# random() lager en tilfeldig desimalverdi mellom 0 og 1
 from random import random
 
 antall_piler = 10000
@@ -10,9 +11,49 @@ på_sirkelen = 0
 utenfor_sirkelen = 0
 
 for pil in range(antall_piler):
-    # random lager en tilfeldig desimalverdi mellom 0 og 1
-    x = 2*random()
-    y = 2*random()
+    # for å få tilfeldige verdier mellom -1 og 1:
+    x = 2*random() - 1
+    y = 2*random() - 1
 
     if (x**2 + y**2) <= 1**2:
-        pass # TODO: fullfør
+        på_sirkelen += 1
+    else:
+        utenfor_sirkelen += 1
+
+andel_på_sirkelen = på_sirkelen / (på_sirkelen + utenfor_sirkelen)
+
+areal_kvadrat = 2 * 2
+areal_sirkel = andel_på_sirkelen * areal_kvadrat
+
+print("Areal av sirkelen:", areal_sirkel, "kvadratmeter")
+
+# Nivå 2: Puslespill
+
+# TODO: Nettsiden virker ikke lenger? Videresendes til noe helt annet?
+
+# Nivå 3: Feilsøking
+
+# random() lager en tilfeldig desimalverdi mellom 0 og 1
+from random import random
+
+antall_piler = 10000
+
+på_sirkelen = 0
+utenfor_sirkelen = 0
+
+for pil in range(antall_piler):
+    # for å få tilfeldige verdier mellom -1 og 1:
+    x = 2*random() - 1
+    y = 2*random() - 1
+
+    if x + y <= 1:
+        på_sirkelen += 1
+    else:
+        utenfor_sirkelen += 1
+
+andel_på_sirkelen = på_sirkelen / utenfor_sirkelen
+
+areal_kvadrat = 2 * 2
+areal_sirkel = areal_kvadrat / andel_på_sirkelen
+
+print("Areal av sirkelen:", areal_sirkel, "kvadratmeter")
