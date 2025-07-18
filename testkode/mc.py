123f81d981a30db4a6d1e7b452594f258ac4ee9b
Odd Petter Sand <o.p.sand@astro.uio.no>
2025-07-18 16:55:13 +0200

    oppgave nivå 1, 3, 4

--- testkode/mc.py
+++ testkode/mc.py
@@ -25,7 +25,7 @@ andel_på_sirkelen = på_sirkelen / (på_sirkelen + utenfor_sirkelen)
 areal_kvadrat = 2 * 2
 areal_sirkel = andel_på_sirkelen * areal_kvadrat
 
-print("Areal av sirkelen:", areal_sirkel, "kvadratmeter")
+print(f"Areal av sirkelen: {areal_sirkel} kvadratmeter")
 
 # Nivå 2: Puslespill
 
@@ -55,5 +55,32 @@ andel_på_sirkelen = på_sirkelen / utenfor_sirkelen
 
 areal_kvadrat = 2 * 2
 areal_sirkel = areal_kvadrat / andel_på_sirkelen
+
+print("Areal av sirkelen:", areal_sirkel, "kvadratmeter")
+
+# Nivå 4: Fyll inn
+
+# random() lager en tilfeldig desimalverdi mellom 0 og 1
+from random import random
+
+antall_piler = 10000
+
+på_sirkelen = # fyll inn
+utenfor_sirkelen = # fyll inn
+
+for pil in range(antall_piler):
+    # for å få tilfeldige verdier mellom -1 og 1:
+    x = # fyll inn
+    y = # fyll inn
+
+    if # fyll inn
+        på_sirkelen += 1
+    else:
+        utenfor_sirkelen += 1
+
+andel_på_sirkelen = på_sirkelen / (på_sirkelen + utenfor_sirkelen)
+
+areal_kvadrat = 2 * 2
+areal_sirkel = # fyll inn
 
 print("Areal av sirkelen:", areal_sirkel, "kvadratmeter")
